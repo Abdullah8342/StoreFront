776358bad1b827fbb3cb1d25a36fbf8e034f993b
Abdullah <abdullah.4110r.work@gmail.com>
2025-11-05 16:45:35 +0500

    Solving The Problem Of Hashing In The Databse Due To Customizing User Model

--- StoreFront/Account/admin.py
+++ StoreFront/Account/admin.py
@@ -1,5 +1,35 @@
+'''
+Decorators
+'''
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import AbstractUser
 # Register your models here.
 
-admin.site.register(AbstractUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    '''
+    Docstring for CustomUserAdmin
+    '''
+    model = AbstractUser
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    'email',
+                    'first_name',
+                    'last_name',
+                    "usable_password",
+                    "password1",
+                    "password2"
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(AbstractUser,CustomUserAdmin)
