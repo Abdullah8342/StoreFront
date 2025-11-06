2856e82933c48704579d499bac8af264dc875c88
Abdullah <abdullah.4110r.work@gmail.com>
2025-11-06 17:27:47 +0500

    Start django rest framework

--- StoreFront/StoreFront/settings.py
+++ StoreFront/StoreFront/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'StoreFront.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
